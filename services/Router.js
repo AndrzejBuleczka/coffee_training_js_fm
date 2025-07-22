fa3d43966f459bfb29a4e34580da92a54b8ab65e
Andrzej Bułeczka <andrzej.buleczka@blacklabel.net>
2025-07-22 15:29:10 +0200

    Add Router service to handle navigation and page rendering

---
+++ services/Router.js
@@ -0,0 +1,49 @@
+const Router = {
+  init: () => {
+    document.querySelectorAll("a.navlink").forEach((link) => {
+      link.addEventListener("click", (event) => {
+        event.preventDefault();
+        const route = link.getAttribute("href");
+        Router.go(route);
+      });
+    });
+
+    //check the initial URL
+    Router.go(location.pathname);
+  },
+
+  go: (route, addToHistory = true) => {
+    if (addToHistory) {
+      history.pushState({ route }, "", route);
+    }
+    let pageElement = null;
+    switch (route) {
+      case "/":
+        pageElement = document.createElement("h1");
+        pageElement.textContent = "Menu";
+        break;
+      case "/order":
+        pageElement = document.createElement("h1");
+        pageElement.textContent = "Your Order";
+        break;
+      default:
+        if (route.startsWith("/product-")) {
+          pageElement = document.createElement("h1");
+          pageElement.textContent = "Details";
+          pageElement.dataset.productId = route.substring(
+            route.lastIndexOf("-") + 1
+          );
+        }
+        break;
+    }
+    if (pageElement) {
+      document.querySelector("main").innerHTML = "";
+      document.querySelector("main").appendChild(pageElement);
+    }
+
+    window.scrollX = 0;
+    window.scrollY = 0;
+  }
+};
+
+export default Router;
