5f7d16501959a6cd32fd57c1964cbe05fd74ed8d
Andrzej Bułeczka <andrzej.buleczka@blacklabel.net>
2025-07-24 16:16:57 +0200

    Implement product details rendering and cart functionality in DetailsPage and ProductItem components; enhance Menu service with product retrieval

--- components/DetailsPage.js
+++ components/DetailsPage.js
@@ -1,3 +1,6 @@
+import { getProductById } from "../services/Menu.js";
+import { addToCart } from "../services/Order.js";
+
 export default class DetailsPage extends HTMLElement {
   constructor() {
     super();
@@ -16,5 +19,29 @@ export default class DetailsPage extends HTMLElement {
     }
     loadCSS();
   }
+
+  async renderData() {
+    if (this.dataset.productId) {
+      this.product = await getProductById(this.dataset.productId);
+      this.root.querySelector("h2").textContent = this.product.name;
+      this.root.querySelector("img").src = `/data/images/${this.product.image}`;
+      this.root.querySelector(".description").textContent =
+        this.product.description;
+      this.root.querySelector(
+        ".price"
+      ).textContent = `$ ${this.product.price.toFixed(2)} ea`;
+      this.root.querySelector("button").addEventListener("click", () => {
+        addToCart(this.product.id);
+        app.router.go("/order");
+      });
+    } else {
+      alert("Invalid Product ID");
+    }
+  }
+
+  connectedCallback() {
+    this.renderData();
+  }
 }
+
 customElements.define("details-page", DetailsPage);
